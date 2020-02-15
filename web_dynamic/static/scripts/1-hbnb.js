dfe5630d781c8c5fa5f6ca5ffcf742507efb8d0c
alzheimeer <fogniebla@hotmail.com>
2020-02-14 23:47:43 -0500

    fix task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
 $(function () {
-  const listAmenities = {};
+  const dictionary = {};
 
   $('div.amenities li input').change(
     function () {
       if ($(this).is(':checked')) {
-        listAmenities[($(this).attr('data-id'))] = $(this).attr('data-name');
+        dictionary[($(this).attr('data-id'))] = $(this).attr('data-name');
       } else {
-        delete listAmenities[($(this).attr('data-id'))];
+        delete dictionary[($(this).attr('data-id'))];
       }
-      $('div.amenities h4').html(Object.values(listAmenities).join(', ') || '&nbsp;');
+      $('div.amenities h4').html(Object.values(dictionary).join(', ') || '&nbsp;');
     });
 });
