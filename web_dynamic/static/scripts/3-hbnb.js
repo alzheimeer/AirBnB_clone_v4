8d4e8c526f2460263f6190fe5c59df37ae60e70c
Hugo Vargas <vargas88hugo@gmail.com>
2020-02-16 13:30:00 -0500

    [Feature] Created task 4 but is incompleted

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,31 @@
+$(function () {
+  const dictionary = {};
+  $('div.amenities li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        dictionary[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete dictionary[($(this).attr('data-id'))];
+      }
+      $('div.amenities h4').html(Object.values(dictionary).join(', ') || '&nbsp;');
+    });
+
+  const url = 'http://0.0.0.0:5001/api/v1/status/'
+  $.getJSON(url, (data) => {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    data: JSON.stringify({}),
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    success: function(data) {
+      console.log(data);
+    }
+  });
+});
