c6efe7ff5bb69260e2429507c428fbc3d3e034fd
alzheimeer <fogniebla@hotmail.com>
2020-02-16 14:02:09 -0500

    feature structure ajax

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -24,8 +24,41 @@ $(function () {
     data: JSON.stringify({}),
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     contentType: 'application/json',
-    success: function(data) {
-      console.log(data);
+    success: data => {
+      for (const place of data) {
+        const template = `<article>
+      <div class="title">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">
+    $${place.price_by_night}
+          </div>
+        </div>
+        <div class="information">
+          <div class="max_guest">
+    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+    <br />
+    ${place.max_guest} Guests
+        </div>
+          <div class="number_rooms">
+    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+    <br />
+    ${place.number_rooms} Bedrooms
+        </div>
+        <div class="number_bathrooms">
+    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+    <br />
+    ${place.number_bathrooms} Bathroom
+        </div>
+      </div>
+    <!-- **********************
+    USER
+    **********************  -->
+      <div class="description">
+        ${place.description}
+      </div>
+    </article> <!-- End 1 PLACE Article -->`;
+        $('section.places').append(template);
+      }
     }
   });
 });
