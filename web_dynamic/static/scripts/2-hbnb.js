5a25597459da4768b50aa673be55bceca8e8e505
alzheimeer <fogniebla@hotmail.com>
2020-02-15 22:50:44 -0500

    feature add task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(function () {
+  const dictionary = {};
+  $('div.amenities li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        dictionary[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete dictionary[($(this).attr('data-id'))];
+      }
+      $('div.amenities h4').html(Object.values(dictionary).join(', ') || '&nbsp;');
+    });
+
+  const url = 'http://0.0.0.0:5001/api/v1/status/'
+  $.getJSON(url, (data) => {
+    if (data.status === 'sucess') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+});
