71705c22490adaa6b01d624c74ac246da7996487
alzheimeer <fogniebla@hotmail.com>
2020-02-16 15:31:50 -0500

    feaure search button

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,7 +10,7 @@ $(function () {
       $('div.amenities h4').html(Object.values(dictionary).join(', ') || '&nbsp;');
     });
 
-  const url = 'http://0.0.0.0:5001/api/v1/status/'
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.getJSON(url, (data) => {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -10,7 +10,7 @@ $(function () {
       $('div.amenities h4').html(Object.values(dictionary).join(', ') || '&nbsp;');
     });
 
-  const url = 'http://0.0.0.0:5001/api/v1/status/'
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.getJSON(url, (data) => {
     if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
@@ -18,4 +18,51 @@ $(function () {
       $('DIV#api_status').removeClass('available');
     }
   });
+
+  $('button').click(() =>
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify({ amenities: Object.keys(dictionary) }),
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      success: data => {
+        $('section.places').empty();
+        $('section.places').append('<h1>Places</h1>');
+        for (const place of data) {
+          const template = `<article>
+
+      <div class="title">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">
+    $${place.price_by_night}
+          </div>
+        </div>
+        <div class="information">
+          <div class="max_guest">
+    <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+    <br />
+    ${place.max_guest} Guests
+        </div>
+          <div class="number_rooms">
+    <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+    <br />
+    ${place.number_rooms} Bedrooms
+        </div>
+        <div class="number_bathrooms">
+    <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+    <br />
+    ${place.number_bathrooms} Bathroom
+        </div>
+      </div>
+    <!-- **********************
+    USER
+    **********************  -->
+      <div class="description">
+        ${place.description}
+      </div>
+    </article> <!-- End 1 PLACE Article -->`;
+          $('section.places').append(template);
+        }
+      }
+    }));
 });
